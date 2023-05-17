5a7af618f533e17f1d2424bf522228f7745d8a6c
illuminoplanet <illuminoplanet@gmail.com>
2023-05-17 09:22:19 +0900

    FEAT: Add extract largest from multiple markers

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import cv2
 import cv2.aruco as aruco
+import numpy as np
 
 
 def detect_marker(frame, aruco_dict, parameters):
     corners, ids, _ = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
     return corners, ids
+
+def extract_main(corners, ids):
+    main_idx = np.argmax([cv2.contourArea(c) for c in corners])
+    return corners[main_idx], ids[main_idx]
+
 
 
 def main():
@@ -19,11 +25,12 @@ def main():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
         corners, ids = detect_marker(gray, aruco_dict, parameters)
-        if ids is None: 
-            break
+        if ids is not None: 
+            corner, id = extract_main(corners, ids)
+            print(id)
 
-        # frame = aruco.drawDetectedMarkers(frame, corners, ids)
-        # cv2.imshow('frame', frame)
+        frame = aruco.drawDetectedMarkers(frame, corners, ids)
+        cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
